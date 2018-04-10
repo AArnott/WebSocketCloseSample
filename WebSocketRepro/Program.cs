0836c91c1ec5f682e0b77d10c088e23b9f87cddd
Andrew Arnott <andrewarnott@gmail.com>
2018-04-10 12:17:50 -0700

    Add sample console host of Kestrel to demonstrate WebSocket crash on close

--- WebSocketRepro/Program.cs
+++ WebSocketRepro/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
 
 namespace WebSocketRepro
 {
@@ -6,7 +13,46 @@ namespace WebSocketRepro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            TestServer testServer = InitializeServer();
+            var webSocket = await EstablishWebSocket(testServer);
+            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Client initiated close", CancellationToken.None); // this throws!
+        }
+
+        private static async Task<WebSocket> EstablishWebSocket(TestServer testServer)
+        {
+            var testClient = testServer.CreateWebSocketClient();
+            return await testClient.ConnectAsync(testServer.BaseAddress, CancellationToken.None);
+        }
+
+        private static TestServer InitializeServer()
+        {
+            IWebHostBuilder webHostBuilder = WebHost.CreateDefaultBuilder(Array.Empty<string>())
+                .UseStartup<Startup>();
+            return new TestServer(webHostBuilder);
+        }
+
+        private class Startup
+        {
+            public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+            {
+                app.Use(async (context, next) =>
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        var buffer = WebSocket.CreateServerBuffer(4096);
+                        var msg = await webSocket.ReceiveAsync(buffer, context.RequestAborted); // wait for the close message
+                        webSocket.Dispose();
+                    }
+
+                    await next();
+                });
+            }
         }
     }
 }
